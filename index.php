3fc3947caa669e0a279e34041c6c0614c6e175a8
Bassim <bassim.matar@gmail.com>
2019-03-09 17:48:29 +0100

    ok

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
 <title>OOP 2 SAS compilator</title>
-<link rel="stylesheet" href="/main.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link rel="stylesheet" href="/main.css?t=<?=time()?>">
 <link rel="stylesheet" href="/codemirror/lib/codemirror.css">
 <link rel="stylesheet" href="/codemirror/theme/lia.css">
 <body>
-<textarea id="oop_code" class="main_part"
+<a href="https://github.com/Bassim789/oop2sas" target="_blanck" id="github_btn">
+    <img src="/github-logo.jpg">
+</a>
+<textarea id="source_code" class="main_part"
     placeholder="Enter OOP SAS code..." style="display: none;"></textarea>
-<textarea id="sas_code" class="main_part" style="display: none;"></textarea>
+<textarea id="compiled_code" class="main_part" style="display: none;"></textarea>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="/codemirror/lib/codemirror.js"></script>
@@ -15,5 +19,9 @@
 <script src="/codemirror/mode/javascript/javascript.js"></script>
 <script src="/codemirror/mode/lia/lia.js"></script>
 <script src="/codemirror/placeHolder.js"></script>
-<script src="/main.js"></script>
+<script src="/main.js?t=<?=time()?>"></script>
+<script>
+var o2s = new oop2sas()
+o2s.init('source_code', 'compiled_code', (source_code) => o2s.run_oop2sas(source_code))
+</script>
 </html>
--- main.js
+++ main.js
@@ -1,91 +1,86 @@
-function oop2sas(oop_code){
-    var clean_lines = [];
-    var sas_code = ''
-    var class_name_found = false
-    var first_method_done = false
-
-    function endswith(str, suffix) {
-        return str.indexOf(suffix, str.length - suffix.length) !== -1
-    }
-    function check_class_name(line){
-        return endswith(line.trim(' '), ':') && !class_name_found
-    }
-    function check_method_name(line){
-        return endswith(line.trim(), ':')
-    }
-    function check_last_line(line_num){
-        return line_num === clean_lines.length - 1
-    }
-    function print_class_name(line){
-        class_name_found = true;
-        class_name = line.split(':')[0].trim()
-        sas_code += '%macro ' + class_name
-        sas_code += `(id, method, arg1, arg2, arg3, arg4, arg5);
-    %local self; %let self =  ${class_name};
+function oop2sas(){
+    this.clean_source_lines = [];
+    this.compiled_code = ''
+    this.class_name_found = false
+    this.first_method_done = false
+    this.class_name = false
+    this.endswith = (str, suffix) => str.indexOf(suffix, str.length - suffix.length) !== -1
+    this.check_class_name = (line) => this.endswith(line.trim(' '), ':') && !this.class_name_found
+    this.check_method_name = (line) => this.endswith(line.trim(), ':')
+    this.check_last_line = (line_num) => line_num === this.clean_source_lines.length - 1
+    this.print_class_name = (line) => {
+        this.class_name = line.split(':')[0].trim()
+        this.class_name_found = true;
+        this.compiled_code += '%macro ' + this.class_name
+        this.compiled_code += `(id, method, arg1, arg2, arg3, arg4, arg5);
+    %local self; %let self =  ${this.class_name};
     %local this; %let this = &self&id;
     %goto &method;
     `
     }
-    function print_method_name(line){
-        if(first_method_done) sas_code += `    %return;
+    this.print_method_name = (line) => {
+        if(this.first_method_done) this.compiled_code += `    %return;
     `
-        sas_code += '%' + line.trim() + `
+        this.compiled_code += '%' + line.trim() + `
 `
-        first_method_done = true
+        this.first_method_done = true
     }
-    function print_line(line){
-        sas_code += line + `
+    this.print_line = (line) => {
+        this.compiled_code += line + `
 `
      }
-     function print_end_class(){
-        sas_code += `    %return;
+    this.print_end_class = () => {
+        this.compiled_code += `    %return;
 %mend;`
     }
-    function run(oop_code){
-        clean_lines = []
+    this.run_oop2sas = (oop_code) => {
+        this.compiled_code = ''
+        this.class_name_found = false
+        this.first_method_done = false
+        this.class_name = false
+        this.clean_source_lines = []
         var lines = oop_code.split('\n')
         for(var i = 0; i < lines.length; i++){
             if(lines[i].trim() !== ''){
-                clean_lines.push(lines[i])
+                this.clean_source_lines.push(lines[i])
             }
         }
-        for(var i = 0; i < clean_lines.length; i++){
-            var line = clean_lines[i]
+        for(var i = 0; i < this.clean_source_lines.length; i++){
+            var line = this.clean_source_lines[i]
             if(line.trim() === ''){
-                if(check_last_line(i)) print_end_class()
+                if(this.check_last_line(i)) this.print_end_class()
                 continue
             }
-            if(check_class_name(line)) print_class_name(line)
-            else if(check_method_name(line)) print_method_name(line)
-            else print_line(line)
-            if(check_last_line(i)) print_end_class()
+            if(this.check_class_name(line)) this.print_class_name(line)
+            else if(this.check_method_name(line)) this.print_method_name(line)
+            else this.print_line(line)
+            if(this.check_last_line(i)) this.print_end_class()
         }
+        return this.compiled_code
     }
-    run(oop_code)
-    return sas_code
+    this.init = (source_id, compiled_id, action) => {
+        var textarea = document.getElementById(source_id)
+        this.editor_source = CodeMirror.fromTextArea(textarea, {
+            mode: "javascript",
+            lineNumbers: true,
+            indentUnit: 4,
+            indentWithTabs: true,
+            viewportMargin: Infinity,
+
+        })
+        this.editor_source.setSize('50%', 'auto') 
+        this.editor_source.on('change', () => {
+            this.editor_compiled.setValue(action(this.editor_source.getValue()))
+        })
+        var textarea_compiled = document.getElementById(compiled_id)
+        this.editor_compiled = CodeMirror.fromTextArea(textarea_compiled, {
+            mode: "javascript",
+            lineNumbers: true,
+            indentUnit: 4,
+            indentWithTabs: true,
+            viewportMargin: Infinity,
+
+        })
+        this.editor_compiled.setSize('50%', 'auto')
+    }    
 }
-$(function(){
-    var textarea = document.getElementById('oop_code')
-    editor = CodeMirror.fromTextArea(textarea, {
-        mode: "javascript",
-        lineNumbers: true,
-        indentUnit: 4,
-        indentWithTabs: true,
-        viewportMargin: Infinity,
-
-    })
-    editor.setSize('50%', 'auto') 
-    editor.on('change', function() {
-        editor_sas.setValue(oop2sas(editor.getValue()))
-    })
-    var textarea_sas = document.getElementById('sas_code')
-    editor_sas = CodeMirror.fromTextArea(textarea_sas, {
-        mode: "javascript",
-        lineNumbers: true,
-        indentUnit: 4,
-        indentWithTabs: true,
-        viewportMargin: Infinity,
-
-    })
-    editor_sas.setSize('50%', 'auto') 
-})
